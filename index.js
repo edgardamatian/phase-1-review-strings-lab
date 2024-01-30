a7f72e072b80f1412f1522495133c6d253f6a751
Edgar Damatian <damatian.e@gmail.com>
2024-01-29 19:49:10 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+console.log(currentUser);
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+  const firstInitial = currentUser.charAt(0);
+  const shortGreeting = "Welcome, " + firstInitial + "!";
+
